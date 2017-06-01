7f84206417b166932d86d695807efda7b4729150
Shishir <shishirmax@gmail.com>
2017-06-01 14:52:03 +0530

    code update

--- app.js
+++ app.js
@@ -15,15 +15,24 @@ app.post("/",function(req,res){
 	if(req.files){
 		var file = req.files.fileUpload,
 			filename = file.name;
+			var temp = filename.split(".")
+			var final = temp[0]+"_PTO."+temp[1];
 			console.log(file);
-		file.mv("./upload/"+filename,function(err){
-			if(err){
-				console.log(err)
-				res.send("error occured"+err)
+			console.log(temp[1]);
+			if(temp[1] == "csv"){
+					file.mv("./upload/"+final,function(err){
+				if(err){
+					console.log(err)
+					res.send("error occured"+err)
+				}
+				else{
+					res.send("Done....");
+					}
+				})
 			}
 			else{
-				res.send("Done....")
+				res.send("Wrong File Format.");
 			}
-		})
+		
 	}
 })
